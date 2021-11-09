686749f79b75cb65eea37b07b9ad52421a49f67a
ciiiii <allen.ccccnm@gmail.com>
2021-11-09 18:09:51 +0800

    replace apt source with mirror

--- environment/container/docker/setup.go
+++ environment/container/docker/setup.go
@@ -3,21 +3,34 @@ package docker
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/abiosoft/colima/config"
 	"path/filepath"
+
+	"github.com/abiosoft/colima/config"
 )
 
 func (d dockerRuntime) setupInVM() error {
 	// install in VM
-	err := d.guest.Run("sudo", "apt-get", "-y", "install", "apt-transport-https", "ca-certificates", "curl", "gnupg", "lsb-release")
+	err := d.guest.Run("sudo", "sed", "-i", "s@http://.*archive.ubuntu.com@http://mirrors.ustc.edu.cn@g", "/etc/apt/sources.list")
 	if err != nil {
 		return fmt.Errorf("error installing in VM: %w", err)
 	}
-	err = d.guest.Run("bash", "-c", "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg")
+	err = d.guest.Run("sudo", "sed", "-i", "s@http://.*security.ubuntu.com@http://mirrors.ustc.edu.cn@g", "/etc/apt/sources.list")
 	if err != nil {
 		return fmt.Errorf("error installing in VM: %w", err)
 	}
-	err = d.guest.Run("bash", "-c", "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null")
+	err = d.guest.Run("sudo", "apt-get", "update")
+	if err != nil {
+		return fmt.Errorf("error installing in VM: %w", err)
+	}
+	err = d.guest.Run("sudo", "apt-get", "-y", "install", "apt-transport-https", "ca-certificates", "curl", "gnupg", "lsb-release")
+	if err != nil {
+		return fmt.Errorf("error installing in VM: %w", err)
+	}
+	err = d.guest.Run("bash", "-c", "curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg")
+	if err != nil {
+		return fmt.Errorf("error installing in VM: %w", err)
+	}
+	err = d.guest.Run("bash", "-c", "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null")
 	if err != nil {
 		return fmt.Errorf("error installing in VM: %w", err)
 	}
